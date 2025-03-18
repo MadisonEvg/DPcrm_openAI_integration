f51608904b23c54fe535d603161143d7c2286f26
Evgeniy M <bbstef@gmail.com>
2025-03-18 20:13:42 +1000

    added crm api

--- routes/webhook.py
+++ routes/webhook.py
@@ -4,12 +4,14 @@ from utils.wazzup_client import WazzupClient
 from utils.openai_client import OpenAIClient
 from utils.statistics_manager import StatisticsManager
 from utils.reminder_tasks import schedule_task
+from utils.dp_client import DpCRMClient
 
 
 webhook_bp = Blueprint('webhooks', __name__)
 wazzup_client = WazzupClient()
 openai_client = OpenAIClient()
 stats_manager = StatisticsManager()
+dp_crm_client = DpCRMClient()
        
 
 @webhook_bp.route('/webhooks', methods=['POST'])
@@ -18,7 +20,7 @@ async def webhook():
     try:
         data = request.get_json(silent=True)
         print(f"Получен вебхук: {data}")
-
+        
         if data and data.get("test") == True:
             print("Тестовый запрос от Wazzup обработан успешно")
             return jsonify({"status": "ok"}), 200
@@ -30,15 +32,23 @@ async def webhook():
             if message.get("type") == "text" and message.get("status") == "inbound":
                 client_message = message.get("text")
                 chat_id = message.get("chatId")
+                lead = dp_crm_client.get_or_create_lead_by_phone(chat_id)
+
+                if lead['status'] not in (dp_crm_client.status_first, dp_crm_client.status_archive):
+                    return jsonify({"status": "ok"}), 200
                 
                 if not client_message or not chat_id:
-                    # print("Нет текста или chatId, игнорируем")
+                    print("Нет текста или chatId, игнорируем")
                     continue
-                print(f"Received message: {client_message}")
+
                 
                 final_response, input_tokens, output_tokens = await openai_client.create_gpt4o_response(
                     client_message, chat_id
                 )
+                
+                if final_response.strip().endswith("статус ожидает звонка"):
+                    final_response = final_response.rstrip().removesuffix("статус ожидает звонка").rstrip()
+                    dp_crm_client.change_lead_to_success_status(lead['id'])
                 
                 wazzup_client.send_message(chat_id, final_response)
                 schedule_task(chat_id)  # Планируем задачу в фоне
@@ -49,7 +59,7 @@ async def webhook():
                     phone_number=chat_id
                 )
             
-        return jsonify({"status": "ok"}), 200  # Отправляем успешный ответ
+        return jsonify({"status": "ok"}), 200
 
     except Exception as e:
         print(f"Ошибка при обработке вебхука: {str(e)}")
--- utils/dp_client.py
+++ utils/dp_client.py
@@ -3,55 +3,82 @@ from config import Config
 
 class DpCRMClient:
     def __init__(self):
-        self.url = Config.AMOCRM_URL
-        self.access_token = Config.AMOCRM_ACCESS_TOKEN
+        self.url = Config.DPCRM_API_URL
+        self.access_token = Config.DPCRM_ACCESS_TOKEN
         self.headers = {
             "Authorization": f"Bearer {self.access_token}",
             "Content-Type": "application/json"
         }
-
-    def get_deal_by_id(self, lead_id):
-
-        url = f"{self.url}/api/v4/leads/{lead_id}"
+        self.status_first = self.get_users_status_by_title(Config.DPCRM_FIRST_STATUS)
+        self.status_success = self.get_users_status_by_title(Config.DPCRM_SUCCESS_STATUS)
+        self.status_archive = self.get_users_status_by_title(Config.DPCRM_ARCHIVE_STATUS)
+        
+    def get_users_status_by_title(self, title):
+        statuses = self.get_users_statuses()['statuses']
+        status_id = next((status['id'] for status in statuses if status['title'] == title), None)
+        return status_id
+    
+    def get_users_statuses(self):
+        url = f"{self.url}/leads/statuses"
         response = requests.get(url, headers=self.headers)
-
         if response.status_code == 200:
-            lead_data = response.json()
-            status_id = lead_data.get("status_id")
-            return status_id
+            return response.json()
         else:
             return f"Ошибка при получении данных: {response.text}"
+        
+    def get_or_create_lead_by_phone(self, phone):
+        response = self.get_lead_by_phone(phone)
+        if response['status'] == 'error' and response['text'] == 'Клиента с таким номером телефона не найдено':
+            user = self.add_user(phone)
+            self.change_user_status(user['lead_id'], self.status_first)
+            response = self.get_lead_by_phone(phone)
+        return response['lead']
+        
+    
+    def get_lead_by_phone(self, phone):
+        url = f"{self.url}/leads/get-by-phone?phone={phone}"
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"Ошибка при получении данных: {response.text}"
+    
+    def add_user(self, phone):
+        """Add user in CRM
 
+        Args:
+            phone (str): user phone number
 
-    def change_deal_status(self, deal_id, new_status_id):
-        url = f"{self.url}/api/v4/leads/{deal_id}"
+        Returns:
+            dict: {'lead_id': 2064666, 'is_new_lead': 1, 'warnings': [], 'status': 'ok'}
+        """
+        url = f"{self.url}/leads"
         payload = {
-            "status_id": new_status_id
+            "phone": phone,
+            "name": f"Контакт {phone}",
+            # "status_id": self.status_first
+        }
+        response = requests.post(url, json=payload, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return f"Ошибка при получении данных: {response.text}"
+        
+    def change_user_status(self, user_id, status_id):
+        url = f"{self.url}/leads/{user_id}/status"
+        payload = {
+            "status_id": status_id
         }
 
         response = requests.patch(url, json=payload, headers=self.headers)
 
         if response.status_code == 200:
-            print(f"Статус сделки с ID {deal_id} успешно обновлен на {new_status_id}.")
+            result = response.json()
+            print(f"Статус пользователя изменён на {result}.")
             return response.json()
         else:
-            print(f"Ошибка при изменении статуса сделки: {response.text}")
+            print(f"Ошибка при изменении статуса клиента: {response.text}")
             return None
-
-    def get_contact_phone(self, contact_id):
-
-        url = f"{self.url}/api/v4/contacts/{contact_id}"
-        response = requests.get(url, headers=self.headers)
-        if response.status_code == 200:
-            try:
-                contact_data = response.json()
-                for field in contact_data.get("custom_fields_values", []):
-                    if field.get("field_code") == "PHONE":
-                        phone = field.get("values", [])[0].get("value", "").lstrip('+')
-                        return phone if phone else "Телефон не найден"
-            except ValueError:
-                print("Ошибка: ответ не в формате JSON. Текст ответа:", response.text)
-                return None
-        else:
-            print("Ошибка при получении данных:", response.text)
-            return None
+    
+    def change_lead_to_success_status(self, user_id):
+        self.change_user_status(user_id, self.status_success)
